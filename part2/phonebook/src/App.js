2acbb23acd707f3675172fc225a89435727435fa
jinjia <jinjiakarl@outlook.com>
2022-09-09 11:15:58 +0300

    feat: part2
    Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // array.concat() returns a new array
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((element => {
+        return (
+          <div key={element.name}>{element.name}</div>
+        )
+      }))}
+    </div>
+  )
+}
+
+export default App
