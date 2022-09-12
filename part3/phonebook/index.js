4bd1c658e433b930d1c902c4db33dc8ac23c2288
jinjia <jinjiakarl@outlook.com>
2022-09-12 22:40:05 +0300

    feat: part3
    Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 // parse body of requests as JSON
 app.use(express.json())
+// app.use(function (req, res, next) {
+//     console.log(req)
+//     morgan.token('body', function (req, res) { 
+//         return JSON.stringify(req.body)
+//     })
+//     next()
+// })
+
+// 如果在路由处添加了req.boy.id，但是这个中间件应该是在进入路由之前的执行的，为什么req.body.id会有值？？？
+// 因为 morgan()还有第二个参数option,{ immediate: true }，默认是false, 意味着等到有response的时候才会打印log
+// 所以如果修改了req.body.id，那么打印的log就是修改后的值
+app.use(morgan(function (tokens, req, res) {
+    let msgs = [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+      ]
+    if (tokens.method(req, res) === 'POST') {
+        // msgs.push(tokens['body'](req, res))
+        msgs.push(JSON.stringify(req.body))
+    }
+    return msgs.join(' ')
+  }))
 
 let persons = [
     {
@@ -41,7 +67,6 @@ const generateRandomId = () => {
     return Math.floor(Math.random() * (max - min)) + min
 }
 
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
@@ -72,20 +97,25 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     // Without the json-parser, the body property would be undefined. 
-    const person = request.body
-    if (!person.name || !person.number) {
+    // let newPerson = request.body // 不要直接修改 request.body，因为它是一个对象，对象是引用类型，修改它会影响到原对象
+    // newPerson.id = generateRandomId()
+    // 因此创建一个新对象，然后修改新对象
+    let newPerson = {
+        id: generateRandomId(),
+        ...request.body
+    }
+    if (!newPerson.name || !newPerson.number) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    if (persons.find(p => p.name === person.name)) {
+    if (persons.find(p => p.name === newPerson.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
-    person.id = generateRandomId()
-    persons = persons.concat(person)
-    response.json(person)
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
 })
 
 app.get('/info', (request, response) => {
