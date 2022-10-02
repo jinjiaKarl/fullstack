32c8345dcf637468c7422a12831dcbf6ebdce163
jinjia <jinjiakarl@outlook.com>
2022-10-02 23:36:19 +0300

    feat: part8
    8.16 user and logging in

--- part8/graphql_server_basic/index.js
+++ part8/graphql_server_basic/index.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose')
 const config= require('./utils/config')
 const Book = require('./models/book')
 const Author = require('./models/author')
-const book = require('./models/book')
+const User = require('./models/users')
+const jwt = require('jsonwebtoken')
 
 console.log('connecting to', config.MONGODB_URI)
-
+// n Mongo, the identifying field of an object is called _id
+// and we previously had to parse the name of the field to id ourselves. 
+// Now GraphQL can do this automatically.
 mongoose.connect(config.MONGODB_URI)
 	.then(() => {
 		console.log('connected to MongoDB')
@@ -18,6 +21,14 @@ mongoose.connect(config.MONGODB_URI)
 
 // 定义schema
 const typeDefs = gql`
+type User {
+  username: String!
+  favoriteGenre: String!
+  id: ID!
+}
+type Token {
+  value: String!
+}
   type Book {
     title: String!
     published: Int!
@@ -36,6 +47,7 @@ const typeDefs = gql`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
   input AuthorInput {
     name: String!
@@ -52,6 +64,14 @@ const typeDefs = gql`
       name: String!
       setBornTo: Int
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 // 定义resolver
@@ -82,10 +102,12 @@ const resolvers = {
     },
     allAuthors: async () => {
       const res = await Author.find({})
-      console.log(res)
       return res
+    },
+    me: (root, args, context) => {
+      return context.currentUser
     }
-  },
+  }, // end of Query
   // 为了解决bookCount的问题，需要定义一个新的resolver
   Author: {
       bookCount: async (root) => {
@@ -94,9 +116,12 @@ const resolvers = {
         const books = await Book.find({ author: author.id })
         return books.length
       }
-    },
+    }, // end of Author
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
       const foundBook = await Book.findOne({ title: args.title })
       if (foundBook) {
         throw new UserInputError('Title must be unique', {
@@ -126,7 +151,10 @@ const resolvers = {
       }
       return book
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
       const foundAuthor = await Author.findOne({ name: args.name })
       if (!foundAuthor) {
         return null
@@ -143,13 +171,46 @@ const resolvers = {
           invalidArgs: args.title,
         })
       }
-    }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+      try{
+        const res = await user.save()
+        return res
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+      if (!user || args.password !== '123') {
+        throw new UserInputError("wrong credentials")
+      }
+      const userForToken = {
+        username: user.username,
+        id: user.id
+      }
+      return { value: jwt.sign(userForToken, config.JWT_SECRET) }
   }
-}
+} // end of Mutation
+} // end of resolvers
 
+// The object returned by context is given to all resolvers as their third parameter. 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), config.JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return {currentUser}
+    }
+  }
 })
 
 server.listen({port: 4000}).then(({ url }) => {
--- part8/graphql_server_basic/utils/config.js
+++ part8/graphql_server_basic/utils/config.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 let MONGODB_URI = process.env.NODE_ENV === 'test'
 	? process.env.TEST_MONGODB_URI
 	: process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 module.exports = {
 	MONGODB_URI,
+	JWT_SECRET
 }
