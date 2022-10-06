cfd5d03d83c41a9a66cdcefa3660ddc507a729ce
jinjia <jinjiakarl@outlook.com>
2022-10-06 22:31:37 +0300

    feat: part7
    7.6: anecdotes and hooks step3

--- part7/anecdotes_router/src/components/CreateNew.js
+++ part7/anecdotes_router/src/components/CreateNew.js
@@ -1,38 +1,47 @@
-import {useState} from 'react'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
-  
+    const contentField = useField('text')
+    const authorField = useField('text')
+    const infoField = useField('text')
   
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: contentField.value,
+        author: authorField.value,
+        info: infoField.value,
         votes: 0
       })
     }
-  
+    const handleReset = () => {
+      contentField.reset()
+      authorField.reset()
+      infoField.reset()
+    }
+    // Invalid value for prop `reset` on <input> tag console warning.
+    //  <input name='content' {...contentField}/>
+    // <input name='author' {...authorField}/>
+    // <input name='info' {...infoField}/>
+    // how to fix?
     return (
       <div>
         <h2>create a new anecdote</h2>
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input name='content' value={contentField.value} type={contentField.type} onChange={contentField.onChange}/>
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input name='author' value={authorField.value} type={authorField.type} onChange={authorField.onChange} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input name='info' value={infoField.value} type={infoField.type} onChange={infoField.onChange}/>
           </div>
-          <button>create</button>
+          <button type='submit'>create</button>
+          <button type='reset' onClick={handleReset}>reset</button>
         </form>
       </div>
     )
